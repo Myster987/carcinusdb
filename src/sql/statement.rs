fb0f904830f5d5c00faabeef4283e5d0f4fc1ee0
Mikołaj Maciejak <myster123987@gmail.com>
2025-07-06 22:01:38 +0200

    add Statement logic and basic SELECT structure

--- src/sql/statement.rs
+++ src/sql/statement.rs
@@ -1,8 +1,74 @@
-
-
+/// SQL statement
+#[derive(Debug)]
 pub enum Statement {
-    Select,
+    Select {
+        columns: Vec<Expression>,
+        from: String,
+        r#where: Option<Expression>,
+        order_by: Option<Vec<Expression>>,
+    },
     Insert,
     Update,
     Delete,
+}
+
+/// Used in select, insert, update, delete.
+#[derive(Debug)]
+pub enum Expression {
+    Identifier(String),
+
+    Value(Value),
+
+    Wildcard,
+
+    BinaryOperation {
+        left: Box<Expression>,
+        operator: BinaryOperator,
+        rigth: Box<Expression>,
+    },
+    UnaryOperation {
+        operator: UnaryOperator,
+        expr: Box<Expression>,
+    },
+    Nested(Box<Expression>),
+}
+
+#[derive(Debug)]
+pub enum Value {
+    String(String),
+    Bool(bool),
+
+    Number(i128),
+}
+
+#[derive(Debug)]
+pub enum BinaryOperator {
+    Eq,
+    Neq,
+    Lt,
+    Gt,
+    LtEq,
+    GtEq,
+    Add,
+    Sub,
+    Mul,
+    Div,
+    And,
+    Or,
+}
+
+#[derive(Debug)]
+pub enum UnaryOperator {
+    Plus,
+    Minus,
+}
+
+/// SQL data types
+#[derive(Debug)]
+pub enum DataType {
+    SmallInt,
+    Int,
+    BigInt,
+    Boolean,
+    VarChar(usize),
 }
