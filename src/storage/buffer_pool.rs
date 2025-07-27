c0128fc68ebd48a82914894edba1746e53777905
Mikołaj Maciejak <myster123987@gmail.com>
2025-07-27 10:33:50 +0200

    wrap BufferPool in Mutex for safe thread sharing

--- src/storage/buffer_pool.rs
+++ src/storage/buffer_pool.rs
@@ -1,28 +1,30 @@
-use std::{cell::RefCell, pin::Pin};
+use std::pin::Pin;
+
+use parking_lot::Mutex;
 
 use crate::utils::buffer::BufferData;
 
 /// Holds free buffers as pointers to memory.
 pub struct BufferPool {
-    pub free_buffers: RefCell<Vec<BufferData>>,
+    pub free_buffers: Mutex<Vec<BufferData>>,
     page_size: usize,
 }
 
 impl BufferPool {
     pub fn new(page_size: usize) -> Self {
         Self {
-            free_buffers: RefCell::new(Vec::new()),
+            free_buffers: Mutex::new(Vec::new()),
             page_size,
         }
     }
 
     pub fn len(&self) -> usize {
-        self.free_buffers.borrow().len()
+        self.free_buffers.lock().len()
     }
 
     /// Returns free pointer to buffer or allocates new one.
     pub fn get(&self) -> BufferData {
-        let mut free_buffers = self.free_buffers.borrow_mut();
+        let mut free_buffers = self.free_buffers.lock();
         if let Some(buffer) = free_buffers.pop() {
             buffer
         } else {
@@ -32,7 +34,7 @@ impl BufferPool {
 
     /// Adds new free buffer to pool.
     pub fn put(&self, buffer: BufferData) {
-        let mut free_buffers = self.free_buffers.borrow_mut();
+        let mut free_buffers = self.free_buffers.lock();
         free_buffers.push(buffer);
     }
 }
