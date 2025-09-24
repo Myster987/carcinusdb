f0bd7537305aba7f6c55ccf907277e36bf84014f
Mikołaj Maciejak <myster123987@gmail.com>
2025-09-24 23:35:45 +0200

    add some fields to Database struct

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -1,8 +1,12 @@
+use std::sync::Arc;
+
+use parking_lot::Mutex;
+
 use crate::{
     error::DatabaseResult,
+    storage::{buffer_pool::GlobalBufferPool, cache::LruPageCache},
     tcp::server::{TcpServer, connection::Connection},
 };
-
 
 pub async fn run(hostname: String, port: u16) -> DatabaseResult<()> {
     log::info!("Starting CarcinusDB...");
@@ -29,6 +33,24 @@ pub fn handle_connection(conn: Connection) -> DatabaseResult<()> {
 }
 
 #[derive(Debug)]
-pub struct Database {}
+pub struct DatabaseConfig {
+    page_size: usize,
+    cache_capacity: usize,
+    pool_init_size: usize,
+}
 
-impl Database {}
+#[derive(Debug)]
+pub struct Database {
+    global_pool: GlobalBufferPool,
+    cache: Arc<Mutex<LruPageCache>>,
+}
+
+impl Database {
+    pub fn new(config: DatabaseConfig) -> Self {
+        let global_pool = GlobalBufferPool::default(config.page_size, config.pool_init_size);
+        Self {
+            global_pool,
+            cache: Arc::new(Mutex::new(LruPageCache::new(config.cache_capacity))),
+        }
+    }
+}
