b5414264b1d70e54961eaf829280f77717d59e1b
Mikołaj Maciejak <myster123987@gmail.com>
2026-03-08 22:41:34 +0100

    refactor(vm): move seq_scan to new operator module

---
+++ src/vm/operator/seq_scan.rs
@@ -0,0 +1,49 @@
+use crate::{
+    sql::schema::Schema,
+    storage::{
+        btree::{BTreeCursor, DatabaseCursor},
+        wal::transaction::ReadTx,
+    },
+    vm::{
+        self,
+        operator::{Operator, Row},
+    },
+};
+
+pub struct SeqScan<'tx, Tx: ReadTx> {
+    cursor: BTreeCursor<'tx, Tx>,
+    schema: Schema,
+    started: bool,
+}
+
+impl<'tx, Tx: ReadTx> SeqScan<'tx, Tx> {
+    pub fn new(cursor: BTreeCursor<'tx, Tx>, schema: Schema) -> Self {
+        Self {
+            cursor,
+            schema,
+            started: false,
+        }
+    }
+}
+
+impl<'tx, Tx: ReadTx> Operator for SeqScan<'tx, Tx> {
+    fn next(&mut self) -> vm::Result<Option<Row>> {
+        if !self.started {
+            self.started = true;
+            if !self.cursor.seek_first()? {
+                return Ok(None);
+            }
+        } else {
+            if !self.cursor.next()? {
+                return Ok(None);
+            }
+        }
+
+        let record = self.cursor.try_record()?;
+        Ok(Some(record.to_owned()))
+    }
+
+    fn schema(&self) -> &Schema {
+        &self.schema
+    }
+}
