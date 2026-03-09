000637bbb2420b76ed009e699592ab2d93295379
Mikołaj Maciejak <myster123987@gmail.com>
2026-03-09 21:39:56 +0100

    refactor(vm): complete Filter operator without duplication

--- src/vm/operator/filter.rs
+++ src/vm/operator/filter.rs
@@ -1,24 +1,33 @@
 use crate::{
-    sql::{self, parser::statement::Expression, schema::Schema, types::Value},
+    sql::{parser::statement::Expression, types::Value},
     vm::{
         self,
+        expression::resolve_expression_to_value,
         operator::{Operator, Row},
     },
 };
 
-pub struct Filter {
-    child: Box<dyn Operator>,
+pub struct Filter<'tx> {
+    child: Box<dyn Operator + 'tx>,
     predicate: Expression,
 }
 
-impl Operator for Filter {
+impl<'tx> Filter<'tx> {
+    pub fn new(child: Box<dyn Operator + 'tx>, predicate: Expression) -> Self {
+        Self { child, predicate }
+    }
+}
+
+impl<'tx> Operator for Filter<'tx> {
     fn next(&mut self) -> vm::Result<Option<Row>> {
         loop {
             match self.child.next()? {
-                Some(row) => match eval(&self.predicate, &row, self.child.schema())? {
-                    Value::Bool(true) => return Ok(Some(row)),
-                    _ => continue,
-                },
+                Some(row) => {
+                    match resolve_expression_to_value(&row, self.child.schema(), &self.predicate)? {
+                        Value::Bool(true) => return Ok(Some(row)),
+                        _ => continue,
+                    }
+                }
                 None => return Ok(None),
             }
         }
@@ -26,37 +35,5 @@ impl Operator for Filter {
 
     fn schema(&self) -> &crate::sql::schema::Schema {
         self.child.schema()
-    }
-}
-
-fn eval(expr: &Expression, row: &Row, schema: &Schema) -> vm::Result<Value> {
-    match expr {
-        Expression::Value(value) => Ok(value.clone()),
-
-        Expression::Identifier(ident) => {
-            let index = schema
-                .index_of(ident)
-                .ok_or(sql::analyzer::Error::ColumnNotFound(ident.to_owned()))?;
-            Ok(row.get_value(index).to_owned())
-        }
-
-        Expression::BinaryOperation {
-            left,
-            operator,
-            right,
-        } => {
-            let left = eval(left, row, schema)?;
-            let right = eval(right, row, schema)?;
-
-            todo!()
-        }
-
-        Expression::UnaryOperation { operator, expr } => {
-            let value = eval(expr, row, schema)?;
-
-            todo!()
-        }
-
-        _ => todo!(),
     }
 }
