8adb69b68c0555e97cc1eda67a32d5003bdcaa88
Mikołaj Maciejak <myster123987@gmail.com>
2026-03-10 21:49:32 +0100

    add(vm::query_result): implement Debug for QueryResult

--- src/vm/query_result.rs
+++ src/vm/query_result.rs
@@ -1,3 +1,5 @@
+use std::fmt::Debug;
+
 use crate::vm::{
     self,
     operator::{Operator, Row},
@@ -8,8 +10,23 @@ pub enum QueryResult<'tx> {
     RowsAffected(usize),
 }
 
+impl Debug for QueryResult<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Rows(_) => f.write_str("QueryResult::Rows(iterator)"),
+            Self::RowsAffected(affected) => write!(f, "QueryResult::RowsAffected({})", affected),
+        }
+    }
+}
+
 pub struct RowIterator<'tx> {
     operator: Box<dyn Operator + 'tx>,
+}
+
+impl<'tx> RowIterator<'tx> {
+    pub fn new(operator: Box<dyn Operator + 'tx>) -> Self {
+        Self { operator }
+    }
 }
 
 impl<'tx> Iterator for RowIterator<'tx> {
