235c01a9b23fbb3c6217b3bee6e347e60b1eb6f2
Mikołaj Maciejak <myster123987@gmail.com>
2026-04-12 22:21:29 +0200

    add(src::tcp::protocol): implement TcpWrite for Response

--- src/tcp/protocol.rs
+++ src/tcp/protocol.rs
@@ -10,7 +10,12 @@
 
 use std::io::Cursor;
 
-use crate::{sql::schema::Schema, tcp, vm::query_result::QueryResult};
+use crate::{
+    sql::schema::Schema,
+    tcp,
+    utils::bytes::{BytesCursor, VarInt},
+    vm::query_result::QueryResult,
+};
 
 pub trait TcpRead {
     /// This function should validate if incoming request is correct and
@@ -39,5 +44,48 @@ impl<'a> Response<'a> {
             schema,
             query_result,
         }
+    }
+
+    /// For iterator based respond type is "i" and for rows affected, it is
+    /// "a".
+    pub fn response_type(&self) -> u8 {
+        match self.query_result {
+            QueryResult::Rows(_) => b'i',
+            QueryResult::RowsAffected(_) => b'a',
+        }
+    }
+}
+
+impl<'a> TcpWrite for Response<'a> {
+    fn to_bytes(self) -> Vec<u8> {
+        let buffer = vec![self.response_type()];
+
+        let mut cursor = BytesCursor::new(buffer);
+
+        let columns = self.schema.column_names();
+        let columns_len = columns.len() as VarInt;
+
+        cursor.put_varint(columns_len);
+
+        for col in self.schema.column_names() {
+            cursor.put_varint(col.len() as VarInt);
+            cursor.put_bytes(col.as_bytes());
+        }
+
+        match self.query_result {
+            QueryResult::RowsAffected(rows_affected) => cursor.put_varint(rows_affected as VarInt),
+            QueryResult::Rows(row_iterator) => {
+                for row_result in row_iterator {
+                    match row_result {
+                        Ok(row) => cursor.put_bytes(&row.to_bytes()),
+                        Err(_) => todo!(),
+                    }
+                }
+            }
+        }
+
+        cursor.put_bytes(b"\r\n");
+
+        todo!()
     }
 }
