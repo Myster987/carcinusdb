22c682ea9f7d00980746431d10eb634bd6b1a303
Mikołaj Maciejak <myster123987@gmail.com>
2026-04-15 22:23:56 +0200

    add(src::tcp::server::connection): impl Connection::write row iterator handling

--- src/tcp/server/connection.rs
+++ src/tcp/server/connection.rs
@@ -4,7 +4,7 @@ use std::{
 };
 
 use crate::{
-    tcp,
+    tcp::{self, protocol::TcpWrite},
     utils::bytes::{BytesCursor, VarInt},
     vm::query_result::QueryResult,
 };
@@ -28,13 +28,55 @@ impl Connection {
                 let mut cursor = BytesCursor::new(buffer);
 
                 cursor.put_varint(rows_affected as VarInt);
+                cursor.put_bytes(b"\r\n");
 
                 let response = cursor.into_inner();
 
                 self.stream.write_all(&response)?;
             }
             QueryResult::Rows(row_iterator) => {
-                todo!()
+                let buffer = vec![b'i'];
+
+                let mut cursor = BytesCursor::new(buffer);
+
+                let schema = row_iterator.schema();
+
+                cursor.put_varint(schema.len() as VarInt);
+
+                for column in &schema.columns {
+                    TcpWrite::push_to_buffer(column, &mut cursor);
+                }
+
+                let mut buffer = cursor.into_inner();
+
+                self.stream.write_all(&buffer)?;
+
+                buffer.clear();
+
+                let mut cursor = BytesCursor::new(buffer);
+
+                for record_result in row_iterator {
+                    match record_result {
+                        Ok(row) => {
+                            cursor.put_u8(b'o');
+                            TcpWrite::push_to_buffer(&row, &mut cursor);
+                            self.stream.write_all(cursor.get_ref())?;
+                        }
+                        Err(err) => {
+                            cursor.put_u8(b'e');
+                            let error_message = format!("{err}");
+
+                            cursor.put_varint(error_message.len() as VarInt);
+                            cursor.put_bytes(error_message.as_bytes());
+
+                            self.stream.write_all(cursor.get_ref())?;
+                            break;
+                        }
+                    }
+                    cursor.get_mut().clear();
+                }
+
+                self.stream.write_all(b"\r\n")?;
             }
         }
 
