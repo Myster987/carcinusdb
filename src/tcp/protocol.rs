9131e90a2a5208d22120fdff5602716d57ad0801
Mikołaj Maciejak <myster123987@gmail.com>
2026-04-16 23:45:30 +0200

    refactor(src::tcp::protocol): comment some stuff and add new Message struct to be used over tcp

--- src/tcp/protocol.rs
+++ src/tcp/protocol.rs
@@ -4,7 +4,7 @@
 //!
 //! ```text
 //! +-------------------------------------------+
-//! |                 HEADER                    |
+//! |    HEADER - response type ("a" or "i")    |
 //! +-------------------------------------------+
 //! | Schema:                                   |
 //! |   - Schema len - varint                   |
@@ -25,7 +25,11 @@
 
 use std::io::Cursor;
 
-use crate::{sql::schema::Schema, tcp, utils::bytes::BytesCursor, vm::query_result::QueryResult};
+use crate::{
+    tcp,
+    utils::bytes::{BytesCursor, VarInt},
+    vm::query_result::QueryResult,
+};
 
 pub trait TcpRead {
     /// This function should validate if incoming request is correct and
@@ -45,16 +49,12 @@ pub trait TcpWrite {
 }
 
 pub struct Response<'a> {
-    schema: Schema,
     query_result: QueryResult<'a>,
 }
 
 impl<'a> Response<'a> {
-    pub fn new(schema: Schema, query_result: QueryResult<'a>) -> Self {
-        Self {
-            schema,
-            query_result,
-        }
+    pub fn new(query_result: QueryResult<'a>) -> Self {
+        Self { query_result }
     }
 
     /// For iterator based respond type is "i" and for rows affected, it is
@@ -66,3 +66,60 @@ impl<'a> Response<'a> {
         }
     }
 }
+
+pub struct Message {
+    len: usize,
+    r#type: char,
+    data: Vec<u8>,
+}
+
+// impl<'a> TcpWrite for Response<'a> {
+//     fn push_to_buffer<T: AsRef<[u8]> + AsMut<[u8]> + Extend<u8>>(&self, src: &mut BytesCursor<T>) {
+//         src.put_u8(self.response_type());
+
+//         match self.query_result {
+//             QueryResult::RowsAffected(rows_affected) => {
+//                 src.put_varint(rows_affected as VarInt);
+//                 src.put_bytes(b"\r\n");
+//             }
+//             QueryResult::Rows(row_iterator) => {
+//                 let schema = row_iterator.schema();
+
+//                 src.put_varint(schema.len() as VarInt);
+
+//                 for column in &schema.columns {
+//                     TcpWrite::push_to_buffer(column, src);
+//                 }
+
+//                 self.stream.write_all(&buffer)?;
+
+//                 buffer.clear();
+
+//                 let mut cursor = BytesCursor::new(buffer);
+
+//                 for record_result in row_iterator {
+//                     match record_result {
+//                         Ok(row) => {
+//                             cursor.put_u8(b'o');
+//                             TcpWrite::push_to_buffer(&row, &mut cursor);
+//                             self.stream.write_all(cursor.get_ref())?;
+//                         }
+//                         Err(err) => {
+//                             cursor.put_u8(b'e');
+//                             let error_message = format!("{err}");
+
+//                             cursor.put_varint(error_message.len() as VarInt);
+//                             cursor.put_bytes(error_message.as_bytes());
+
+//                             self.stream.write_all(cursor.get_ref())?;
+//                             break;
+//                         }
+//                     }
+//                     cursor.get_mut().clear();
+//                 }
+
+//                 self.stream.write_all(b"\r\n")?;
+//             }
+//         }
+//     }
+// }
