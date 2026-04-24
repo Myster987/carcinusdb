451705b6906ee9b49bf69742301844af4777d857
Mikołaj Maciejak <myster123987@gmail.com>
2026-04-24 23:00:04 +0200

    refactor: change main function to async and add simple client test with user input

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,35 @@ mod tcp;
 mod utils;
 mod vm;
 
-fn main() -> anyhow::Result<()> {
-    simple_logger::init_with_level(log::Level::Info).unwrap();
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    simple_logger::init()?;
     dotenvy::dotenv()?;
 
     let db_path = env::var("CARCINUSDB_PATH")?;
-    let hostname = env::var("CARCINUSDB_HOSTNAME")?;
-    let port = env::var("CARCINUSDB_PORT")?.parse::<u16>()?;
+    let hostname = env::var("CARCINUSDB_HOSTNAME").unwrap_or("127.0.0.1".to_string());
+    let port = env::var("CARCINUSDB_PORT")
+        .unwrap_or("4000".to_string())
+        .parse::<u16>()?;
 
-    database::run(db_path, hostname, port)?;
+    database::run(db_path, hostname, port).await?;
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_client() -> anyhow::Result<()> {
+    use crate::tcp::client::ClientConnection;
+
+    simple_logger::init()?;
+
+    let mut client = ClientConnection::connect("127.0.0.1:4000").await?;
+
+    let sql = utils::io::input("Enter SQL: ")?;
+
+    let query_result = client.query(&sql).await?;
+
+    println!("{query_result}");
 
     Ok(())
 }
